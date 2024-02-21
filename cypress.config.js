04594d9598ad8513db57073b66afb039235cbf3d
SushmithaThoutam905 <sushmitha.thoutam@cognine.com>
2024-02-21 14:26:55 +0530

    reverted changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,8 @@
 const { defineConfig } = require("cypress");
-const MochaJUnitReporter = require("mocha-junit-reporter");
-
+ 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(_, config) {
-      // Register the JUnit reporter
-      const junitReporter = new MochaJUnitReporter({
-        mochaFile: `${config.projectRoot}/test-results/results-[hash].xml`,
-      });
-
-      // Listen for the 'run:end' event to generate the JUnit report
-      const onRunEnd = (runResults) => {
-        if (runResults && runResults.totalFailed > 0) {
-          // Only generate the JUnit report if there are failed tests
-          junitReporter.epilogue(runResults.stats);
-        }
-      };
-
-      // Check if `config.env` exists before using `on`
-      if (config.env && typeof config.env.on === "function") {
-        config.env.on("run:end", onRunEnd);
-      } else {
-        // Fallback in case `config.env.on` is not available
-        config.on("run:end", onRunEnd);
-      }
-    },
-  },
+    setupNodeEvents(on, config) {
+    }
+  }
 });
