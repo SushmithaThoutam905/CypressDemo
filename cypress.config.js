2244db6d7333539ffb92a52874d862c47b5ce8fd
Mujeeb-Cognine <144911937+Mujeeb-Cognine@users.noreply.github.com>
2024-02-21 12:03:09 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,29 @@
 const { defineConfig } = require("cypress");
+const MochaJUnitReporter = require("mocha-junit-reporter");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const junitReporter = new MochaJUnitReporter({
+        mochaFile: "test-results/results-[hash].xml", // Set the path for JUnit XML report
+      });
+
+      on("before:browser:launch", (browser, launchOptions) => {
+        // modify browser launch options
+        return launchOptions;
+      });
+
+      on("task", {
+        // add custom task listener
+      });
+
+      on("after:spec", (spec, results) => {
+        if (results && results.stats) {
+          junitReporter.epilogue(results.stats);
+        }
+      });
+
+      config.reporter = junitReporter;
     },
   },
-});
+});;
