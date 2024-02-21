781319da6d74a34ef976216a8be9ddb5e9fb63ec
SushmithaThoutam905 <sushmitha.thoutam@cognine.com>
2024-02-21 19:55:56 +0530

    reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir:'cypress/results',
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    debug: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
+
     },
   },
 });
