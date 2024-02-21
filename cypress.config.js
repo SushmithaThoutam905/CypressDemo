fed1f3c505721841c42df379aba33ccb473b29c9
SushmithaThoutam905 <sushmitha.thoutam@cognine.com>
2024-02-21 15:53:25 +0530

    updated config

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  screenshotsFolder: "cypress/screenshots",
-  videosFolder: "cypress/videos",
-  video: true,
-  trashAssetsBeforeRuns: true,
 
   e2e: {
     setupNodeEvents(on, config) {
