13d0f0618dc3b80a6ed980339bf3d86d38b78618
SushmithaThoutam905 <sushmitha.thoutam@cognine.com>
2024-02-22 15:57:24 +0530

    added videos

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const currentDate = new Date().toISOString().replace(/:/g, '-');
 const reportFilename = `cypress-report-${currentDate}.html`;
 
 module.exports = defineConfig({
+  video: true, // Enable video recording
+  videoUploadOnPasses: false, // Do not upload videos for passing tests
+  videoCompression: 32, // Adjust video compression quality (optional)
+  videosFolder: 'cypress/videos', // Define the folder to save the videos
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     reportDir:'cypress/results',
@@ -14,7 +18,7 @@ module.exports = defineConfig({
     debug: true,
     overwrite: false,
     reportFilename: reportFilename
-  },
+    },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on)
