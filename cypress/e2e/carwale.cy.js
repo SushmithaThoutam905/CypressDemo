d7026781eee815bfc4ab633edc2301bbce310a04
SushmithaThoutam905 <sushmitha.thoutam@cognine.com>
2024-02-26 19:22:07 +0530

    verifying links

--- cypress/e2e/carwale.cy.js
+++ cypress/e2e/carwale.cy.js
@@ -13,12 +13,27 @@ describe('CarWale Website Tests', () => {
     cy.get('.o-cKuOoN').contains('Honda City').should('be.visible');
   });
 
-  it('should get elements from each header', () => {
+  it('should get elements from each header', async() => {
     cy.get('.o-bkmzIL').find('li').children().each(($tab) => {
       cy.wrap($tab).trigger('mouseover').wait(3000);
+      const header = $tab.text().trim();
+      cy.log('Tab:', header);
       cy.get('.o-cpnuEd').find('div.oROWc7').each(($el) => {
         const content = $el.text().trim();
-        cy.log('Elements:', content);
+        cy.log('Elements:', content)
+        })
+        if(header === 'NEW CARS'){
+          cy.log("abcdedfg")
+          const urls = cy.fixture('urls').then((urls) => {
+            // Iterate through each URL
+            urls.expectedUrls.forEach((expectedUrl, index) => {
+              // Perform actions with each URL
+              cy.get('.o-bkmzIL').find('li').children().first().trigger('mouseover').wait(2000);
+              cy.get('.o-cpnuEd').find('div.oROWc7').eq(index).click();
+              cy.url().should('include', expectedUrl);
+            });
+          });
+        }
         // cy.wrap($el).click();
         // cy.get('.voh2vm').then(($popup) => {
         //   if ($popup.is(':visible')) {
@@ -27,7 +42,6 @@ describe('CarWale Website Tests', () => {
         // });
     });
   });
-});
   it('should click on each navigation link under new cars', async() => {
     const urls = await cy.fixture('urls').then((urls) => {
       // Iterate through each URL
